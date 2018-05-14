75e4702e5acfcd16192ebfd941b03e844a6c73e5
Elliot Kim <elliotkim916@gmail.com>
2018-05-13 20:10:40 -0700

    added runServer and closeServer function for testing

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
-module.exports = {app};
+function runServer() {
+    const PORT = process.env.PORT || 3000;
+    return new Promise((resolve, reject) => {
+        server = app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+            resolve(server);
+        })
+        .on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+
+module.exports = {app, runServer, closeServer};
