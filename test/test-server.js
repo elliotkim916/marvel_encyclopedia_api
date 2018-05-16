d07b3700651b295522c3343f0a24a9f9f381ebf6
Elliot Kim <elliotkim916@gmail.com>
2018-05-16 08:54:29 -0700

    successfully able to test my POST endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -2,13 +2,15 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
-const should = chai.should();
-chai.use(chaiHttp);
+const {ReadingList} = require('../models/logs');
 
-describe('API', function() {
+describe('Marvel Encyclopedia Server-Side API', function() {
     before(function() {
         return runServer(TEST_DATABASE_URL);
     });
@@ -17,12 +19,73 @@ describe('API', function() {
         return closeServer();
     });
 
-    it('should 200 on GET requests', function() {
+describe('GET endpoint', function() {
+    it('should return entire comic reading list', function() {
         return chai.request(app)
             .get('/api/marvel')
             .then(function(res) {
-                res.should.have.status(200);
-                res.should.be.json;
+                expect(res).to.be.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.lengthOf.at.least(1);
             });
+        });
     });
+
+describe('POST endpoint', function() {
+    it('should add a new reading entry', function() {
+    const newPost = {
+        "title": "Iron Man (2018) #71",
+        "read": "Read Later",
+        "userName": "Stan Lee",
+        "id": "5afbbad4202724320a0d4fa4"
+    }
+    
+    return chai.request(app)
+        .post('/api/marvel')
+        .send(newPost)
+        .then(function(res) {
+            expect(res).to.be.status(201);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.include.keys('title', 'read');
+            expect(res.body.title).to.equal(newPost.title);
+            expect(res.body.read).to.equal(newPost.read);
+            expect(res.body.userName).to.equal(newPost.userName);
+            return ReadingList.findById(res.body.id);
+        })
+// we retreive new post from the db & compare its data to the data we sent over
+        .then(function(post) {
+            expect(post.title).to.equal(newPost.title);
+            expect(post.read).to.equal(newPost.read);
+            expect(post.userName).to.equal(newPost.userName);
+        });
+    });
+});
+
+describe('PUT endpoint', function() {
+    it('should update the fields user chooses', function() {
+        const toUpdate = {
+            read: 'Read Later'
+        }
+
+    return ReadingList
+        .findOne()
+        .then(post => {
+            toUpdate._id = post._id;
+        
+        return chai.request(app)
+            .put(`/api/marvel/${post._id}`)
+            .send(toUpdate);
+        }).then(res => {
+            expect(res).to.have.status(204);
+            return ReadingList.findById(toUpdate._id);
+        })
+// we retrieve the updated post from db and prove the post in db
+// is equal to the updated values we sent over
+        .then(post => {
+            expect(toUpdate.read).to.equal(post.read);
+        });
+    });
+});
 });
