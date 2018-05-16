c33fd53b9b6107efb460a955823c734659a808ba
Elliot Kim <elliotkim916@gmail.com>
2018-05-15 20:16:27 -0700

    calling runServer with the TEST_DATABASE_URL

--- test/test-server.js
+++ test/test-server.js
@@ -4,13 +4,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
-
+const {TEST_DATABASE_URL} = require('../config');
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe('API', function() {
     before(function() {
-        return runServer();
+        return runServer(TEST_DATABASE_URL);
     });
 
     after(function() {
