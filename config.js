9c33f73261a572bc6ac66e961040806035681baf
Elliot Kim <elliotkim916@gmail.com>
2018-05-15 18:56:47 -0700

    exporting database url, test database url, and port

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
 
-module.exports = {
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-};
+// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_encyclopedia';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
+exports.PORT = process.env.PORT || 3000;
