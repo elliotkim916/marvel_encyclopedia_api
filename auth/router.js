9bec35056338e57a8021158c50b8760fea3f7069
Elliot Kim <elliotkim916@gmail.com>
2018-05-16 22:36:46 -0700

    able to createAuthToken and established post requests with router at /login & /refresh

---
+++ auth/router.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const router = express.Router();
+
+const localAuth = passport.authenticate('local', {session: false});
+
+const createAuthToken = user => {
+    return jwt.sign({user}, config.JWT_SECRET, {
+        subject: user.username,
+        expiresIn: config.JWT_EXPIRY,
+        algorithm: 'HS256'
+    });
+};
+
+// when the user provides a valid username & password, we create a JWT
+// the token is then sent back to the user, who can store it and use it
+// to authenticate for subsequent API requests
+router.post('/login', localAuth, (req, res) => {
+    const authToken = createAuthToken(req.user.serialize());
+    res.json({authToken});
+});
+
+const jwtAuth = passport.authenticate('jwt', {session: false});
+
+router.post('/refresh', jwtAuth, (req, res) => {
+    const authToken = createAuthToken(req.user);
+    res.json({authToken});
+});
+
+module.exports = {router};
