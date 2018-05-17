558d176b227a10e04cf92948ab2fc6bab35b7846
Elliot Kim <elliotkim916@gmail.com>
2018-05-16 22:23:31 -0700

    exporting JWT_SECRET & JWT_EXPIRY

--- config.js
+++ config.js
@@ -5,3 +5,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_encyclopedia';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
 exports.PORT = process.env.PORT || 3000;
+
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
