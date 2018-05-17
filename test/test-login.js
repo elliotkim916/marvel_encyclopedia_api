fcfb5fe24e5fd78ce486870d824fce570a90a941
Elliot Kim <elliotkim916@gmail.com>
2018-05-17 11:00:03 -0700

    successfully tested all login endpoints

--- test/test-login.js
+++ test/test-login.js
@@ -17,9 +17,22 @@ const createAccountCredentials = {
     "password": "imademarvel"
 }
 
+function tearDownDb() {
+    console.warn('Deleting database!');
+    return mongoose.connection.db.dropDatabase();
+}
+
 describe('Create Account, Login, & Check Token', function() {
     before(function() {
         return runServer(TEST_DATABASE_URL);
+    });
+
+    beforeEach(function() {
+        return User.create(createAccountCredentials);
+    });
+
+    afterEach(function() {
+        return tearDownDb();
     });
 
     after(function() {
@@ -38,4 +51,43 @@ describe('/POST Create Account', function() {
     });
 });
 
+let authToken;
+describe('/POST Log in to account', function() {
+    it('should log in and receive authToken', function() {
+        chai.request(app)
+        .post('/api/auth/login')
+        .send(createAccountCredentials)
+        .then((err, res) => {
+            expect(res).to.be.status(200);
+            expect(res.body.state).to.be.true;
+            expect(res.body.data).to.be.a('object');
+        })
+    })
+});
+
+describe('/GET Access and use protected page', function() {
+    it('should have access to protected page', function() {
+        chai.request(app)
+        .get('/api/protected')
+        .set('Authorization', `Bearer ${authToken}`)
+        .then((err, res) => {
+            expect(res).to.be.status(200);
+            expect(res.body.state).to.be.true;
+            expect(res.body.data).to.be.a('object');
+        })
+    })
+});
+
+describe('/POST To request new JWT', function() {
+    it('should give a new JWT with a later expiry date', function() {
+        chai.request(app)
+        .post('/api/auth/refresh')
+        .set('Authorization', `Bearer ${authToken}`)
+        .then((err, res) => {
+            expect(res).to.be.status(200);
+            expect(res.body.state).to.be.true;
+            expect(res.body.data).to.be.a('object');
+        })
+    })
+});
 });
