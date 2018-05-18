e073368089eb3f32eb9cd4af7a6c757802e569b0
Elliot Kim <elliotkim916@gmail.com>
2018-05-18 13:32:23 -0700

    changed PORT to 8080 and incorporated client origin

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_encyclopedia';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = process.env.PORT || 8080;
 
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
