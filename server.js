c0d6df07ea40c8f53617cdccb6b4c5429f23baaa
Elliot Kim <elliotkim916@gmail.com>
2018-05-21 16:59:24 -0700

    edited CORS

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header('Access-Control-Allow-Credentials','true'); 
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     if (req.method === 'OPTIONS') { 
         return res.sendStatus(204); 
         } 
-    return next(); 
+    next(); 
 });
 
 passport.use(localStrategy);
