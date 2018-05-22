288e24ad3aacddf7fff11a7b9f9833e9592994a9
Elliot Kim <elliotkim916@gmail.com>
2018-05-22 10:13:38 -0700

    updated client origin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://upbeat-hamilton-aed8e5.netlify.com/';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_encyclopedia';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
