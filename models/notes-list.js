e5ef6f77d437d4ee17212ace727dd94469b40988
Elliot Kim <elliotkim916@gmail.com>
2018-05-23 10:32:35 -0700

    incorporated username into notesListSchema

--- models/notes-list.js
+++ models/notes-list.js
@@ -4,14 +4,16 @@ const mongoose = require('mongoose');
 
 const notesListSchema = mongoose.Schema({
     title: {type: String},
-    note: {type: String}
+    note: {type: String},
+    username: {type: String}
 });
 
 notesListSchema.methods.serialize = function() {
     return {
         title: this.title,
         note: this.note,
-        id: this._id
+        id: this._id,
+        username: this.username
     };
 };
 
