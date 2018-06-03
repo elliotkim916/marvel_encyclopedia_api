13ecbdb8876a03525a45dc2d7e9a7d5ebdbd51c4
Elliot Kim <elliotkim916@gmail.com>
2018-06-02 21:26:22 -0700

    test

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_e
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
 exports.PORT = process.env.PORT || 8080;
 
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'marvel_encyclopedia_access_1004';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
