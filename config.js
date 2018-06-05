b982b9a6f50bdda37c0c6d22ed1cee9209dfdd89
Elliot Kim <elliotkim916@gmail.com>
2018-06-05 10:50:21 -0700

    updated client origin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3001';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_encyclopedia';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
