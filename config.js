bf834b987129455eac97ffd84d355258d8e1a105
Elliot Kim <elliotkim916@gmail.com>
2018-08-03 16:22:47 -0700

    changed client origin back to original

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 'use strict';
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://enigmatic-lowlands-24493.herokuapp.com/';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
-// 'http://localhost:3000'
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/marvel_encyclopedia';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_marvel_encyclopedia';
 exports.PORT = process.env.PORT || 8080;
